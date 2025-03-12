58085926f300c2d798ec9f76a75d861c614c1989
eooce <142894633+eooce@users.noreply.github.com>
2025-03-12 22:21:45 +0800

    fix：Cargo dependencies

--- main.rs
+++ main.rs
@@ -9,15 +9,12 @@ use std::process::Command;
 use tokio::time::{sleep, Duration};
 use base64::engine::general_purpose::STANDARD as BASE64_STANDARD;
 use base64::Engine as _;
-use rand::{thread_rng, Rng};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
 async fn setup_environment() {
-    let mut rng = rand::thread_rng();
-    let random_port: u16 = rng.gen_range(20000..=60000); 
 
     let env_vars = [
         ("UUID", "66e5c8dd-3176-458e-8fb0-1ed91d2f9602"),
